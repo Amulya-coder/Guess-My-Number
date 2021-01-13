2362ac905c8670de648e4b109e6c61d55b20b416
Amulya Dixit <amulyadixit07@gmail.com>
2021-01-13 21:31:00 +0530

    Refactoring Code

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent='🎉 Correct Number';
-
-document.querySelector('.score').textContent='10';
-document.querySelector('.highscore').textContent='50';
-
-document.querySelector('.number').textContent='25';
-
-document.querySelector('.guess').value='23';
-console.log(document.querySelector('.guess').value);*/
 
 let secretNumber=Math.trunc(Math.random()*20)+1;
 
@@ -26,7 +14,7 @@ document.querySelector('.check').addEventListener('click',function(){
     
     //When there is no input    
     if(!guess){
-        // document.querySelector('.message').textContent='⛔ No Number!!';
+        
         displayMessage('⛔ No Number!!');
     }
 
@@ -34,7 +22,7 @@ document.querySelector('.check').addEventListener('click',function(){
     else if(guess===secretNumber){
 
         document.querySelector('.number').textContent=secretNumber;
-        //document.querySelector('.message').textContent='🎉 Correct Number';
+       
         displayMessage('🎉 Correct Number');
         if(highscore<score){
             highscore=score;
@@ -51,43 +39,27 @@ document.querySelector('.check').addEventListener('click',function(){
         //When guess is too high
         if(score>1){
 
-        // document.querySelector('.message').textContent=(guess>secretNumber)?'📈 Too high!':'📉 Too Low!';
+        
         displayMessage((guess>secretNumber)?'📈 Too high!':'📉 Too Low!');
         score--;
         document.querySelector('.score').textContent=score;
         }
         else{
-            // document.querySelector('.message').textContent='💥 You lost the game!'
+            
             displayMessage('💥 You lost the game!')
             document.querySelector('.score').textContent=0;
         }
     }
 
-    //When guess is too low
-    // else if(guess<secretNumber){
-    //     if(score>1){
-    //     document.querySelector('.message').textContent='📉 Too Low!';
-    //     score--;
-    //     document.querySelector('.score').textContent=score;
-    //     }
-    //     else{
-    //         document.querySelector('.message').textContent='💥 You lost the game!'
-    //         document.querySelector('.score').textContent=0;
-    //     }
-    // }
 
 });
 
-//Coding Challenge #1
-// Implement a game rest functionality
 
 document.querySelector('.again').addEventListener('click',function(){
 
     score=20;
     secretNumber=Math.trunc(Math.random()*20)+1;
 
-    
-    // document.querySelector('.message').textContent='Start guessing...';
     displayMessage('Start guessing...');
     document.querySelector('.number').textContent='?';
     document.querySelector('.score').textContent=score;
